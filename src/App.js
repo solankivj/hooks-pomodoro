bb70861604d34035b45720bfa671b0a71a8189f9
Vijay Solanki <40413513+solankivj@users.noreply.github.com>
2020-07-09 22:26:56 +0530

    set startTimer function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 
+const padTime = (time) => {
+  return time.toString().padStart(2, '0')
+}
+
 export default function App() {
+  const [timeLeft, setTimeLeft] = useState(25*60);
+  const [title, setTitle] = useState('Let the countdown begin!!!');
+
+  const startTimer = () => {
+    setInterval(() => {
+      setTimeLeft(timeLeft => {
+        if(timeLeft >= 1) return timeLeft - 1;
+        
+        return  0;
+      })
+    }, 1000)
+  }
+
+  const minutes = padTime(Math.floor(timeLeft / 60));
+  const seconds = padTime((timeLeft - (minutes * 60)))
   return (
     <div className="app">
-      <h2>Pomodoro!</h2>
+      <h2>{title}</h2>
 
       <div className="timer">
-        <span>00</span>
+        <span>{minutes}</span>
         <span>:</span>
-        <span>00</span>
+        <span>{seconds}</span>
       </div>
 
       <div className="buttons">
-        <button>Start</button>
+        <button type="button" onClick={startTimer}>Start</button>
         <button>Stop</button>
         <button>Reset</button>
       </div>
